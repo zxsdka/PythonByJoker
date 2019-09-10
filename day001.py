4e359eaa4287ad4417b837ed1ca51c5e20357f1d
zxsdka <51552489+zxsdka@users.noreply.github.com>
2019-09-10 08:38:51 +0800

    Update day001.py

--- day001.py
+++ day001.py
@@ -100,6 +100,7 @@ print('goodbye, world', end='!\n')
 
 # print('%d'%sum)
 
+9.
 # import math
 
 # r = eval(input('请输入半径：'))
@@ -109,7 +110,7 @@ print('goodbye, world', end='!\n')
 # Area = 5 * side ** 2 / (4 * math.tan(math.pi/5))
 
 # print("面积是：",format(Area,'5.2f'))
-
+10.
 import math
 x1,y1 = eval(input("请输入第一个点的经度和纬度："))
 x2,y2 = eval(input("请输入第二个点的经度和纬度："))
@@ -121,51 +122,7 @@ y22 = math.radians(y2)
 d = radius * math.acos(math.sin(x11) * math.sin(x22) + math.cos(x11) * math.cos(x22) * math.cos(y11-y22))
 print("两点之间距离 %fkm"%d)
 
-f = float(input('请输入华氏温度: '))
-c = (f - 32) / 1.8
-print('%.1f华氏度 = %.1f摄氏度' % (f, c))
 
-import math
-
-radius = float(input('请输入圆的半径: '))
-perimeter = 2 * math.pi * radius
-area = math.pi * radius * radius
-print('周长: %.2f' % perimeter)
-print('面积: %.2f' % area)
-
-a = 5
-b = 10
-c = 3
-d = 4
-e = 5
-a += b
-a -= c
-a *= d
-a /= e
-print("a = ", a)
-
-flag1 = 3 > 2
-flag2 = 2 < 1
-flag3 = flag1 and flag2
-flag4 = flag1 or flag2
-flag5 = not flag1
-print("flag1 = ", flag1)
-print("flag2 = ", flag2)
-print("flag3 = ", flag3)
-print("flag4 = ", flag4)
-print("flag5 = ", flag5)
-print(flag1 is True)
-print(flag2 is not False)
-
-a = 321
-b = 123
-print(a + b)
-print(a - b)
-print(a * b)
-print(a / b)
-print(a // b)
-print(a % b)
-print(a ** b)
 
 
 
